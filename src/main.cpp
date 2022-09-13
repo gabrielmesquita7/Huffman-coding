0b7bc241d4718c09185898feae1faa6664600331
gabrielmesquita7 <xelectrozchannel@gmail.com>
2022-09-13 12:51:47 -0300

    add binary tree

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include "tree.hpp"
+
+int main()
+{
+    int reg[] = {5, 3, 7, 2, 4, 6, 1};
+    Tree *raiz = CreateTree();
+    Tree *aux = CreateTree();
+    Record r;
+
+    cout << "Arvore vazia: " << TVazia(&raiz) << endl;
+
+    cout << "ELEMENTOS DA ARVORE: {";
+
+    for (int i = 0; i < 7; i++)
+    {
+        r.key = reg[i];
+        r.value = 1;
+        insertTree(&raiz, r);
+        cout << " " << reg[i];
+    }
+
+    cout << "}" << endl;
+
+    cout << "Arvore vazia: \n"
+         << TVazia(&raiz) << endl;
+
+    cout << "METODO PREORDEM: {";
+    preordem(raiz);
+    cout << "}\n"
+         << endl;
+
+    cout << "METODO CENTRAL: {";
+    central(raiz);
+    cout << "}\n"
+         << endl;
+
+    cout << "METODO POSORDEM: {";
+    posordem(raiz);
+    cout << "}\n"
+         << endl;
+
+    r.key = 2;
+    pesquisa(&raiz, &aux, r);
+
+    cout << "PESQUISA 2 E IMPRIME A SUBARVORE {";
+    central(aux);
+    cout << "}\n"
+         << endl;
+
+    r.key = 3;
+    removeTree(&raiz, r);
+
+    cout << "REMOVE 3 E IMPRIME A ARVORE { ";
+    central(raiz);
+    cout << "}\n"
+         << endl;
+}
---
+++ src/tree.hpp
@@ -0,0 +1,38 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+typedef struct Record Record;
+typedef struct Tree Tree;
+
+struct Record
+{
+	int key;
+	int value;
+};
+
+struct Tree
+{
+	Record reg;
+	Tree *esq, *dir;
+};
+
+Tree *CreateTree();
+bool TVazia(Tree **t);
+void insertTree(Tree **t, Record r);
+void pesquisa(Tree **t, Tree **aux, Record r);
+int isInTree(Tree *t, Record r);
+
+void removeTree(Tree **t, Record r);
+void antecessor(Tree **t, Tree *aux);
+
+void preordem(Tree *t);
+void central(Tree *t);
+void posordem(Tree *t);
+void widthPath(Tree *t);
+
+#endif
